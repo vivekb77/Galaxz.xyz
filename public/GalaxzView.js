85b3b66e1d0081598c3d50df7012c660630dfb0f
vivek <vivek2020bheke@gmail.com>
2021-12-27 21:41:38 +1300

    added view galaxz web app page

---
+++ public/GalaxzView.js
@@ -0,0 +1,186 @@
+
+function GetGalaxz(){
+
+    const database = firebase.database();
+    var  galaxzArray = [];
+
+    database.ref('/galaxz').orderByChild("status")
+    .equalTo("Active")   // get galaxz which are created by logged in user
+    .once("value",function(ALLRecords){
+        ALLRecords.forEach(
+            function(CurrentRecord) {
+               
+    var galaxzId = CurrentRecord.val().galaxzId;
+    var createdBy = CurrentRecord.val().createdBy;
+    var createdDate = CurrentRecord.val().createdDate;
+    var name = CurrentRecord.val().name;
+    var description = CurrentRecord.val().description;
+    var numberOfSolasys = CurrentRecord.val().numberOfSolasys;
+    var views = CurrentRecord.val().views;
+    var followers = CurrentRecord.val().followers;
+    var shares = CurrentRecord.val().shares;
+    var priority = CurrentRecord.val().priority;
+               
+               
+         // var formatteddate = new Date(createdDate).toDateString();
+
+          var options = { month: 'short', day: 'numeric' };
+           var formatteddate0  = new Date(createdDate);
+           formatteddate = (formatteddate0.toLocaleDateString("en-US", options));
+
+                //create an array , sort on priority
+                
+                 
+               var galaxzObject = 
+                    {"galaxzId":galaxzId,
+                    "createdBy":createdBy,
+                    "formatteddate":formatteddate,
+                    "name":name,
+                    "description":description,
+                    "numberOfSolasys":numberOfSolasys,
+                    "views":views,
+                    "followers":followers,
+                    "shares":shares,
+                    "priority":priority};
+                
+                 galaxzArray.push(galaxzObject)
+                  
+            });
+           //sorting , the higher the priority, the above galaxz appears
+           galaxzArray.sort((a, b) => {
+            return b.priority - a.priority;
+        });
+            AddGalaxzCell(galaxzArray);
+        
+        });
+
+        
+}
+
+function AddGalaxzCell (galaxzArray){
+   
+    var counter = 0;  // to create unique ids for the tags for each galaxz
+
+ for (i=0 ;i < galaxzArray.length; i++){
+   
+   
+var galaxzdiv = document.createElement('div');
+galaxzdiv.className = 'post-preview';
+galaxzdiv.id = 'galaxzdiv'+counter;
+document.getElementById('maindiv').append(galaxzdiv);
+
+var toppara = document.createElement('p');
+toppara.className = 'post-meta';
+toppara.id = 'toppara'+counter;
+document.getElementById('galaxzdiv'+counter).append(toppara);
+
+var curatedBy = document.createElement('span');
+curatedBy.id = 'curatedBy'+counter;
+curatedBy.innerText = galaxzArray[i].createdBy;
+document.getElementById('toppara'+counter).append(curatedBy);
+
+document.getElementById('curatedBy'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ) );
+
+var curatedDate = document.createElement('span');
+curatedDate.id = 'curatedDate'+counter;
+curatedDate.innerText = galaxzArray[i].formatteddate;
+document.getElementById('toppara'+counter).append(curatedDate);
+
+
+var titleDesc = document.createElement('a');
+titleDesc.setAttribute('href', "SolasysView.html/gId="+ galaxzArray[i].galaxzId);
+titleDesc.id = 'titleDesc'+counter;
+document.getElementById('galaxzdiv'+counter).append(titleDesc);
+
+var title = document.createElement('h2');
+title.id = 'post-title'+counter;
+title.className = 'post-title';
+title.innerText = galaxzArray[i].name;
+document.getElementById('titleDesc'+counter).append(title);
+
+var postsubtitle = document.createElement('h3');
+postsubtitle.id = 'post-subtitle'+counter;
+postsubtitle.className = 'post-subtitle';
+postsubtitle.innerText = galaxzArray[i].description;
+document.getElementById('titleDesc'+counter).append(postsubtitle);
+
+var br = document.createElement("br");
+document.getElementById('galaxzdiv'+counter).append(br);
+
+var bottompara = document.createElement('p');
+bottompara.className = 'post-meta';
+bottompara.id = 'bottompara'+counter;
+document.getElementById('galaxzdiv'+counter).append(bottompara);
+
+var solasysBtn = document.createElement('img');
+solasysBtn.id = 'solasysBtn'+counter;
+solasysBtn.className = 'img-bottompara';
+solasysBtn.src = 'assets/noofsolasys.svg';
+document.getElementById('bottompara'+counter).append(solasysBtn);
+
+//document.getElementById('solasysBtn'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0' ) );
+
+
+var solasysval = document.createElement('span');
+solasysval.id = 'solasysval'+counter;
+solasysval.innerText = galaxzArray[i].numberOfSolasys;
+document.getElementById('bottompara'+counter).append(solasysval);
+
+document.getElementById('solasysval'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ) );
+
+var viewsBtn = document.createElement('img');
+viewsBtn.id = 'viewsBtn'+counter;
+viewsBtn.className = 'img-bottompara';
+viewsBtn.src = 'assets/views.svg';
+document.getElementById('bottompara'+counter).append(viewsBtn);
+
+//document.getElementById('viewsBtn'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0' ) );
+
+var viewsval = document.createElement('span');
+viewsval.id = 'viewsval'+counter;
+viewsval.innerText = galaxzArray[i].views;
+document.getElementById('bottompara'+counter).append(viewsval);
+
+document.getElementById('viewsval'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ) );
+
+
+var followsBtn = document.createElement('img');
+followsBtn.id = 'followsBtn'+counter;
+followsBtn.className = 'img-bottompara';
+followsBtn.src = 'assets/like.svg';
+document.getElementById('bottompara'+counter).append(followsBtn);
+
+//document.getElementById('followsBtn'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0' ) );
+
+
+var followsval = document.createElement('span');
+followsval.id = 'followsval'+counter;
+followsval.innerText = galaxzArray[i].followers;
+document.getElementById('bottompara'+counter).append(followsval);
+
+document.getElementById('followsval'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0' ) );
+
+var sharesBtn = document.createElement('img');
+sharesBtn.id = 'sharesBtn'+counter;
+sharesBtn.className = 'img-bottompara';
+sharesBtn.src = 'assets/share.svg';
+document.getElementById('bottompara'+counter).append(sharesBtn);
+
+//document.getElementById('sharesBtn'+counter).appendChild( document.createTextNode( '\u00A0\u00A0\u00A0' ) );
+
+
+var sharesval = document.createElement('span');
+sharesval.id = 'sharesval'+counter;
+sharesval.innerText = galaxzArray[i].shares;
+document.getElementById('bottompara'+counter).append(sharesval);
+
+var hr = document.createElement("hr");
+hr.className='my-4';
+document.getElementById('galaxzdiv'+counter).append(hr);
+
+++counter;
+
+}
+
+}
+
